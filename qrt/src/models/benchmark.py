fb52666083e69b1abcf7ce14eb3a32288dc97c50
OncleBobby <gpontus0807@gmail.com>
2024-01-13 18:45:07 +0100

    improve notebook

--- qrt/src/models/benchmark.py
+++ qrt/src/models/benchmark.py
@@ -13,26 +13,25 @@ class Benchmark1(Model):
 
 import xgboost, numpy
 class Benchmark2(Model):
-  def __init__(self, X_train, y_train, X_valid, y_valid, train_scores):
-    Model.__init__(self, X_train, y_train, X_valid, y_valid, train_scores)    
-    self.model = None
-
+  def __init__(self, X_train, y_train, X_valid, y_valid, train_scores, \
+        params={
+          'booster': 'gbtree',
+          'tree_method':'hist',
+          'max_depth': 8, 
+          'learning_rate': 0.025,
+          'objective': 'multi:softprob',
+          'num_class': 2,
+          'eval_metric':'mlogloss'
+        }):
+    Model.__init__(self, X_train, y_train, X_valid, y_valid, train_scores, params)
+    self.model = None    
   def train(self):
-    params_1 = {
-        'booster': 'gbtree',
-        'tree_method':'hist',
-        'max_depth': 8, 
-        'learning_rate': 0.025,
-        'objective': 'multi:softprob',
-        'num_class': 2,
-        'eval_metric':'mlogloss'
-        }
     d_train = xgboost.DMatrix(self.X_train.replace({0:numpy.nan}), self.y_train)
     d_valid = xgboost.DMatrix(self.X_valid.replace({0:numpy.nan}), self.y_valid)
 
     num_round = 10000
     evallist = [(d_train, 'train'), (d_valid, 'eval')]
-    self.model = xgboost.train(params_1, d_train, num_round, evallist, early_stopping_rounds=100)
+    self.model = xgboost.train(self.params, d_train, num_round, evallist, early_stopping_rounds=100, verbose_eval=0)
   def predict(self, X):
     predictions = pandas.DataFrame(self.model.predict(xgboost.DMatrix(X), iteration_range=(0, self.model.best_iteration)))
     predictions[2] = 0
