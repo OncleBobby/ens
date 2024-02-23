6c839548989408f6dca9bef1a2763bd40418ae7e
OncleBobby <gpontus0807@gmail.com>
2024-02-23 20:14:51 +0100

    clean-up

--- qrt/src/models/xgboost_classifier_model.py
+++ qrt/src/models/xgboost_classifier_model.py
@@ -1,31 +1,13 @@
-import pandas, xgboost, logging
-from sklearn import model_selection
+import pandas, xgboost
 from .model import Model
 
 class XgboostClassifierModel(Model):
-  def __init__(self, X_train, y_train, train_scores, \
-        params={
-          'booster': 'gbtree',
-          'tree_method':'hist',
-          'max_depth': 8, 
-          'learning_rate': 0.025,
-          'objective': 'multi:softprob',
-          'num_class': 3,
-          'eval_metric':'mlogloss'
-        }):
+  def __init__(self, X_train, y_train, train_scores, params):
     Model.__init__(self, X_train, y_train, train_scores, params)
     self.model = None    
   def get_model(self):
     params = self.params.copy()
-    return xgboost.XGBClassifier(
-        booster=params['booster'],
-        tree_method=params['tree_method'],
-        max_depth=params['max_depth'],    
-        learning_rate=params['learning_rate'],
-        objective=params['objective'],
-        num_class=params['num_class'],
-        eval_metric=params['eval_metric']
-    )
+    return xgboost.XGBClassifier(**params)
   def fit(self):
     self.model = self.get_model()
     self.model.fit(self.X_train, self.format_y(self.y_train))
